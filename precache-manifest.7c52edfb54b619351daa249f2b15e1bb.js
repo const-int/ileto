7e20c9b33f4dd034bab361b155976784b7c83908
Konstantin Naumov <const.int.naumov@gmail.com>
2020-07-24 05:05:15 +0200

    Updates

---
+++ precache-manifest.7c52edfb54b619351daa249f2b15e1bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c20ab6d7d538be379f94636a5939f9b",
+    "url": "/ileto/index.html"
+  },
+  {
+    "revision": "cf8d705732acd67fdd66",
+    "url": "/ileto/static/css/main.2437aabe.chunk.css"
+  },
+  {
+    "revision": "e4bd3e1fbb1552819b54",
+    "url": "/ileto/static/js/2.e888a3bd.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ileto/static/js/2.e888a3bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8d705732acd67fdd66",
+    "url": "/ileto/static/js/main.70cb4c4b.chunk.js"
+  },
+  {
+    "revision": "1c3f22b39fff0ed1e3a1",
+    "url": "/ileto/static/js/runtime-main.22004439.js"
+  },
+  {
+    "revision": "f7a0c31e13069b62fbdf68a957a0fc51",
+    "url": "/ileto/static/media/Lato-Bold.f7a0c31e.woff"
+  },
+  {
+    "revision": "cd6c6aac15595c65e75760bce456198a",
+    "url": "/ileto/static/media/Lato-Regular.cd6c6aac.woff"
+  },
+  {
+    "revision": "bef8639602379476aaade35f341782b6",
+    "url": "/ileto/static/media/Lato-Semibold.bef86396.woff"
+  }
+]);
