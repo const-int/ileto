433bc170a104c168b133c4f7a828203e9dbfbdfe
Konstantin Naumov <const.int.naumov@gmail.com>
2020-09-14 23:10:25 +0200

    Updated swap button styles

--- src/components/Shared/IconButton/Secondary/useStyles.js
+++ src/components/Shared/IconButton/Secondary/useStyles.js
@@ -2,6 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   root: {
+    position: "relative",
     width: 40,
     height: 40,
     minWidth: [[40], "!important"],
@@ -15,10 +16,29 @@ export default makeStyles((theme) => ({
     zIndex: 20,
 
     "&:active": {
-      backgroundColor: "#dad7d4"
+      backgroundColor: "#dad7d4",
+
+      "&::after": {
+        opacity: 1,
+      }
+    },
+
+    "&::after": {
+      opacity: 0,
+      content: "''",
+      position: "absolute",
+      top: -10,
+      left: -10,
+      right: -10,
+      bottom: -10,
+      backgroundColor: "#dad7d4",
+      borderRadius: "50%",
+      transition: "opacity .15s ease",
     }
   },
   label: {
+    position: "relative",
+    zIndex: 2,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
