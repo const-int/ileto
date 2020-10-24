629e26c4582c989e542c6578af5d5cbf4cab0bc3
Konstantin Naumov <const.int.naumov@gmail.com>
2020-10-24 02:14:29 +0200

    Uncommented Display component

--- src/components/MainScreen/index.jsx
+++ src/components/MainScreen/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { ValueContextProvider } from "context/ValueContext";
 import Container from "./Container";
 import Separator from "./Separator";
-// import Display from "./Display";
+import Display from "./Display";
 import Dial from "./Dial";
 
 function MainScreen({ isMenuOpen }) {
   return (
     <Container isHidden={isMenuOpen}>
       <ValueContextProvider>
-        {/* <Display /> */}
+        <Display />
         <Separator />
         <Dial />
       </ValueContextProvider>
