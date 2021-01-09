ff2faf38b0ce0fec793ae0efdb2ca9a4ca3cddf1
Konstantin Naumov <const.int.naumov@gmail.com>
2021-01-09 17:12:10 +0200

    Updated spacer height

--- src/components/CurrencySelectScreen/BottomGradient/useStyles.js
+++ src/components/CurrencySelectScreen/BottomGradient/useStyles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   root: {
-    height: 400,
+    height: 265,
     background: `linear-gradient(180deg, #FFFFFF 0%, ${theme.color.primary} 100%);`,
     opacity: .5
   }
