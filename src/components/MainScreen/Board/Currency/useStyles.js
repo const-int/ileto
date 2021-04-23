0a4c5d52ee4bd1909b570f88d9324c729c918ebb
Konstantin Naumov <const.int.naumov@gmail.com>
2021-04-23 23:39:19 +0200

    Update flag positioning

--- src/components/MainScreen/Board/Currency/useStyles.js
+++ src/components/MainScreen/Board/Currency/useStyles.js
@@ -64,7 +64,7 @@ export default makeStyles((theme) => ({
     width: 24,
     height: 18,
     borderRadius: 2,
-    margin: [[-2, 9, 0, 0]],
+    marginRight: 9,
     boxShadow: `0 0 0 1px ${theme.color.flagBorder}`,
     zIndex: 2
   }
