ba94f2a28c04ab04c55ecdb54f154c8aa30c8bf5
Konstantin Naumov <const.int.naumov@gmail.com>
2022-07-24 12:49:52 +0200

    Updates

---
+++ precache-manifest.84f9f1ffa2a90eb923818e938af400ce.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d4ae8b1a5d5acb93d4a7ba10defaae",
+    "url": "/ileto/index.html"
+  },
+  {
+    "revision": "1c2fdc616cbb2d1c33e8",
+    "url": "/ileto/static/css/main.a31f0141.chunk.css"
+  },
+  {
+    "revision": "f98775e1ad9b841e9c79",
+    "url": "/ileto/static/js/2.ee14c63c.chunk.js"
+  },
+  {
+    "revision": "c3e488c1b6c2872635b113f5babd035a",
+    "url": "/ileto/static/js/2.ee14c63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2fdc616cbb2d1c33e8",
+    "url": "/ileto/static/js/main.178f277c.chunk.js"
+  },
+  {
+    "revision": "1c3f22b39fff0ed1e3a1",
+    "url": "/ileto/static/js/runtime-main.22004439.js"
+  },
+  {
+    "revision": "da9f426d49791884643286454844e93e",
+    "url": "/ileto/static/media/0.da9f426d.svg"
+  },
+  {
+    "revision": "0e2c7f29f2f548785ae85e161e49aa3f",
+    "url": "/ileto/static/media/1.0e2c7f29.svg"
+  },
+  {
+    "revision": "b3b1cb664a77c275a39873ce7feb335a",
+    "url": "/ileto/static/media/2.b3b1cb66.svg"
+  },
+  {
+    "revision": "1b660d001e521f4b1e14fb9b7477958d",
+    "url": "/ileto/static/media/3.1b660d00.svg"
+  },
+  {
+    "revision": "d407fac5de00ed17e37c03f963ea50ed",
+    "url": "/ileto/static/media/4.d407fac5.svg"
+  },
+  {
+    "revision": "b2e7d14f04cd5bbd6d623f5b4451bd2f",
+    "url": "/ileto/static/media/5.b2e7d14f.svg"
+  },
+  {
+    "revision": "9acf17cba4257bccb9832aa50a2ec67f",
+    "url": "/ileto/static/media/6.9acf17cb.svg"
+  },
+  {
+    "revision": "9e87da935c1f747dd429758c5845fc40",
+    "url": "/ileto/static/media/7.9e87da93.svg"
+  },
+  {
+    "revision": "a0122710053fa1b387f5f6a9c3976ba1",
+    "url": "/ileto/static/media/8.a0122710.svg"
+  },
+  {
+    "revision": "1387608a34f4474695ec6d5c93b71a88",
+    "url": "/ileto/static/media/9.1387608a.svg"
+  },
+  {
+    "revision": "f7a0c31e13069b62fbdf68a957a0fc51",
+    "url": "/ileto/static/media/Lato-Bold.f7a0c31e.woff"
+  },
+  {
+    "revision": "cd6c6aac15595c65e75760bce456198a",
+    "url": "/ileto/static/media/Lato-Regular.cd6c6aac.woff"
+  },
+  {
+    "revision": "bef8639602379476aaade35f341782b6",
+    "url": "/ileto/static/media/Lato-Semibold.bef86396.woff"
+  },
+  {
+    "revision": "26b54381a126e6ba118d9c111cd00550",
+    "url": "/ileto/static/media/app-icon.26b54381.svg"
+  },
+  {
+    "revision": "29e6a87752b155286660d1d4ad5dfe29",
+    "url": "/ileto/static/media/facebook.29e6a877.svg"
+  },
+  {
+    "revision": "23ca6295ca6ae16e1c9c96d3ae9a91e7",
+    "url": "/ileto/static/media/share.23ca6295.svg"
+  },
+  {
+    "revision": "2caef882f119ab5ff749c7ae61c84da8",
+    "url": "/ileto/static/media/telegram.2caef882.svg"
+  },
+  {
+    "revision": "6e928be43e4ad58e99eec709699bd8ff",
+    "url": "/ileto/static/media/twitter.6e928be4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ileto/precache-manifest.c77ba3330d6005992ac95d298ba58f0a.js"
+  "/ileto/precache-manifest.84f9f1ffa2a90eb923818e938af400ce.js"
 );
 
 self.addEventListener('message', (event) => {
